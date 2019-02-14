959260d6f3d52f57150100f3a92b85a065b4c8ab
殷春雨 <yinchunyu@jf.com>
2019-02-14 17:26:28 +0800

    '修改配置'

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 const Config = {
-  appid: 'wx3c2699af4b7fba2f',                          // 小程序
-  appsecret: 'b310762067bbb1e3bb0dcbab9cffed4a',        // 小程序 appsecret
+  appid: 'wxb508e00368e14fd4',                          // 小程序
+  appsecret: '76aa8cf82163a2298228af8ada75d312',        // 小程序 appsecret
   wxAppid: 'wx0677735d7b0c81a2',                        // 公众号 appid
   wxAppsecret: '6621e0e549c04cadd25495d8d890e1cd'       // 公众号 appsecret
 }
